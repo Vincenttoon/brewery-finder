57e30004d65ee188aea2b1e4808f858d737c8c39
rafiqnasrat <rafiqullahnasrat3@gmail.com>
2022-08-31 11:00:23 -0500

    this all done for me

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,49 @@
 // variables from HTML
-const saveArray = [];
 const wellInformation = ["Stop 1", "Stop 2", "Stop 3", "Stop 4", "Stop 5", "Stop 6", "Stop 7", "Stop 8"];
 const breweryContainerEl = document.querySelector('#brewery-container');
 const searchButton = document.querySelector('#search-btn');
+let city = "";
 // button action to take input data and feed to brewery by city API
 
 
 // need to make this a reusable function
 // function to grab data from breweries by city
 $('#search-btn').on('click', function() {
-    const city = $("#city").val().trim();
+
+    // get value from input
+     city = $("#city").val().trim();
+
+     if(city == "")
+     {
+         alert("Please provide a city name!");
+     }else
+     {
+        let saveArray = [];
+    
+        if(localStorage.getItem("searchHistory") != null)
+        {
+            saveArray = JSON.parse(localStorage.getItem("searchHistory"));
+        }
+    
+        // push data to array
+        saveArray.push(city);
+    
+        // push data to array
+        localStorage.setItem("searchHistory", JSON.stringify(saveArray))
+    
+        // call load data function
+        loadData(city);
+     }
+})
 
 
-    const apiUrlCity = `https://api.openbrewerydb.org/breweries?by_city=${city}&size=20`
+
+
+
+// Function to load data
+function loadData(myCity)
+{
+    const apiUrlCity = `https://api.openbrewerydb.org/breweries?by_city=${myCity}&size=20`
 
     fetch(apiUrlCity)
     // console.log(apiUrlCity);
@@ -31,12 +62,44 @@ $('#search-btn').on('click', function() {
         
     });
 
-    // Save city searches?
-    // const searchCont = $('#search-container');
-    // const cityBtn = $('<button>').text(city);
+ 
+}
 
-    // searchCont.append(cityBtn);
-})
+
+
+
+
+// load search history
+function loadSearchHistory()
+{
+    // get data from local storage convert it to array
+    let searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
+
+    // Append all searched history
+    $(".search-result").html("");
+    searchHistory.forEach(function(value, key){
+        if( key < 10)
+        {
+            $(".search-result").append("<p class='sbtn'>"+value+"</p>");
+        }
+    });
+}
+
+
+// check if there is any value in local storage to load
+if(localStorage.getItem("searchHistory") != null)
+{
+    loadSearchHistory();
+}
+
+// search by history buttons
+$(".sbtn").each(function(value, key){
+    $(this).click(function(){
+        loadData($(this).text())
+    });
+});
+
+
 
 // function to grab data from city and display on page
 
@@ -78,21 +141,18 @@ const renderBreweryCards = (brewery) => {
         breweryUrl: saveUrl
     }
 
-    saveArray.push(saveData);
-
-    saveInfo(saveData);
-    // displayMap()
+    loadSearchHistory();
 
 }
 
 // Save info on each card with button click if possible?
 
 
-function saveInfo (saveData) {
-    $('#save-btn').on('click', function(){
-        localStorage.setItem(saveData.breweryName, JSON.stringify(saveData));
-    });
-}
+// function saveInfo (saveData) {
+//     $('#save-btn').on('click', function(){
+//         localStorage.setItem(saveData.breweryName, JSON.stringify(saveData));
+//     });
+// }
 
 // parse saved data to page below buttons
 
